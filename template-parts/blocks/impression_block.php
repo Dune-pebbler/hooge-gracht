7d03825605a4367fe5cd1a5ca308795eade328ff
Tim-DP <tim@dunepebbler.nl>
2025-09-18 09:20:35 +0200

    impression block tweak

--- template-parts/blocks/impression_block.php
+++ template-parts/blocks/impression_block.php
@@ -5,6 +5,9 @@ $inside_impression = get_sub_field('inside_impression');
 $outside_text = get_sub_field('outside_text');
 $inside_text = get_sub_field('inside_text');
 $block_title = get_sub_field('block_title');
+// Determine availability of impressions
+$has_outside = !empty($outside_impression) && !empty($outside_impression['url']);
+$has_inside = !empty($inside_impression) && !empty($inside_impression['url']);
 ?>
 <section class="impression_block" id="situatie">
     <div class="container-fluid">
@@ -16,23 +19,36 @@ $block_title = get_sub_field('block_title');
                 </div>
             </div>
         </div>
-        <div class="row justify-content-center impression-banner">
-            <div class="col-12 col-md-6 col-lg-4 p-0 ">
-                <div class="impression_block__outside-image-container slide-left-on-scroll">
-                    <img src="<?= $outside_impression['url']; ?>" alt="<?= $outside_impression['alt']; ?>">
-                </div>
-                <div class="impression_block__text-container slide-left-on-scroll">
-                    <h2><?= $outside_text; ?></h2>
-                </div>
+        <?php if ($has_outside || $has_inside): ?>
+            <div class="row justify-content-center impression-banner">
+                <?php if ($has_outside): ?>
+                    <div class="col-12 col-md-6 col-lg-4 p-0 ">
+                        <div
+                            class="impression_block__outside-image-container<?= (!$has_inside ? ' single' : ''); ?> slide-left-on-scroll">
+                            <img src="<?= $outside_impression['url']; ?>" alt="<?= $outside_impression['alt']; ?>">
+                        </div>
+                        <?php if (!empty($outside_text)): ?>
+                            <div class="impression_block__text-container slide-left-on-scroll">
+                                <h2><?= $outside_text; ?></h2>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                <?php endif; ?>
+
+                <?php if ($has_inside): ?>
+                    <div class="col-12 col-md-6 col-lg-4 p-0 ">
+                        <div
+                            class="impression_block__inside-image-container<?= (!$has_outside ? ' single' : ''); ?> slide-right-on-scroll">
+                            <img src="<?= $inside_impression['url']; ?>" alt="<?= $inside_impression['alt']; ?>">
+                        </div>
+                        <?php if (!empty($inside_text)): ?>
+                            <div class="impression_block__text-container slide-right-on-scroll">
+                                <h2><?= $inside_text; ?></h2>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                <?php endif; ?>
             </div>
-            <div class="col12 col-md-6 col-lg-4 p-0 ">
-                <div class="impression_block__inside-image-container slide-right-on-scroll">
-                    <img src="<?= $inside_impression['url']; ?>" alt="<?= $inside_impression['alt']; ?>">
-                </div>
-                <div class="impression_block__text-container slide-right-on-scroll">
-                    <h2><?= $inside_text; ?></h2>
-                </div>
-            </div>
-        </div>
+        <?php endif; ?>
     </div>
 </section>
